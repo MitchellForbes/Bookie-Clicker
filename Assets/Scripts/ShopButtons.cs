66d3ad038ac226d5ee24d14296d9272bfd74e3db
MitchellForbes Home <135566058+MitchellForbes@users.noreply.github.com>
2023-10-03 17:13:27 +1100

    Shopbuttons Effects added

--- Assets/Scripts/ShopButtons.cs
+++ Assets/Scripts/ShopButtons.cs
@@ -5,16 +5,33 @@ using UnityEngine;
 public class ShopButtons : MonoBehaviour
 {
     BookClicking bookCheck;
-    public int knowledge;
+    public float knowledge;
 
+    public float passKnowledge;
 
     public int priceItemOne;
+    public int priceItemTwo;
+    public int priceItemThree;
+    public int priceItemFour;
+
+    private bool Itemcheck;
+
     // Start is called before the first frame update
     void Start()
     {
         bookCheck = GameObject.Find("ClickyBook").GetComponent<BookClicking>();
-
+        //passKnowledge = 1f;
+        StartCoroutine("TimeCheck");
         priceItemOne = 10;
+        priceItemTwo = 20;
+        priceItemThree = 50;
+        priceItemFour = 1000;
+
+        Itemcheck = false;
+    }
+
+    private void Update()
+    {
     }
 
     public void ButtonCheck()
@@ -35,21 +52,44 @@ public class ShopButtons : MonoBehaviour
 
     public void ItemTwo()
     {
-
+        if(priceItemFour <= knowledge)
+        {
+            passKnowledge += 1f;
+            knowledge -= priceItemTwo;
+        }
     }
 
     public void ItemThree()
     {
-
+        if(priceItemThree <= knowledge)
+        {
+            passKnowledge += 2f;
+            knowledge -= priceItemThree;
+        }
+        
     }
 
     public void ItemFour()
     {
-
+        if (Itemcheck == false && priceItemFour <= knowledge)
+        {
+            bookCheck.clickAdd *= 2;
+            Itemcheck = true;
+            knowledge -= priceItemFour;
+        }
     }
 
-    public void ItemFive()
+    IEnumerator TimeCheck()
     {
-
+        for (; ; )
+        {
+            knowledge += passKnowledge;
+            yield return new WaitForSeconds(.2f);
+        }
     }
+
+    //public void ItemFive()
+    //{
+
+    //}
 }
